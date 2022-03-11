fab88f64fbc2f04b478363ad6cd27b386a3bb2c8
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2022-03-11 12:14:27 +0100

    Initial commit

---
+++ ui/src/views/Settings.vue
@@ -0,0 +1,257 @@
+<template>
+  <cv-grid fullWidth>
+    <cv-row>
+      <div class="bx--col-lg-16 page-title">
+        <h2>{{ $t("settings.title") }}</h2>
+      </div>
+    </cv-row>
+    <cv-row v-if="error.getConfiguration">
+      <cv-column>
+        <NsInlineNotification
+          kind="error"
+          :title="$t('action.get-configuration')"
+          :description="error.getConfiguration"
+          :showCloseButton="false"
+        />
+      </cv-column>
+    </cv-row>
+    <cv-row>
+      <cv-column>
+        <cv-tile light>
+          <cv-form @submit.prevent="configureModule">
+            <!-- TODO remove test field and code configuration fields -->
+            <cv-text-input
+              :label="$t('settings.test_filed')"
+              v-model="testField"
+              :placeholder="$t('settings.test_filed')"
+              :disabled="loading.getConfiguration || loading.configureModule"
+              :invalid-message="error.testField"
+              ref="testField"
+            ></cv-text-input>
+            <cv-row v-if="error.configureModule">
+              <cv-column>
+                <NsInlineNotification
+                  kind="error"
+                  :title="$t('action.configure-module')"
+                  :description="error.configureModule"
+                  :showCloseButton="false"
+                />
+              </cv-column>
+            </cv-row>
+            <NsButton
+              kind="primary"
+              :icon="Save20"
+              :loading="loading.configureModule"
+              :disabled="loading.getConfiguration || loading.configureModule"
+              >{{ $t("settings.save") }}</NsButton
+            >
+          </cv-form>
+        </cv-tile>
+      </cv-column>
+    </cv-row>
+  </cv-grid>
+</template>
+
+<script>
+import to from "await-to-js";
+import { mapState } from "vuex";
+import {
+  QueryParamService,
+  UtilService,
+  TaskService,
+  IconService,
+} from "@nethserver/ns8-ui-lib";
+
+export default {
+  name: "Settings",
+  mixins: [TaskService, IconService, UtilService, QueryParamService],
+  pageTitle() {
+    return this.$t("settings.title") + " - " + this.appName;
+  },
+  data() {
+    return {
+      q: {
+        page: "settings",
+      },
+      urlCheckInterval: null,
+      testField: "", // TODO remove
+      loading: {
+        getConfiguration: false,
+        configureModule: false,
+      },
+      error: {
+        getConfiguration: "",
+        configureModule: "",
+        testField: "", // TODO remove
+      },
+    };
+  },
+  computed: {
+    ...mapState(["instanceName", "core", "appName"]),
+  },
+  beforeRouteEnter(to, from, next) {
+    next((vm) => {
+      vm.watchQueryData(vm);
+      vm.urlCheckInterval = vm.initUrlBindingForApp(vm, vm.q.page);
+    });
+  },
+  beforeRouteLeave(to, from, next) {
+    clearInterval(this.urlCheckInterval);
+    next();
+  },
+  created() {
+    this.getConfiguration();
+  },
+  methods: {
+    async getConfiguration() {
+      this.loading.getConfiguration = true;
+      this.error.getConfiguration = "";
+      const taskAction = "get-configuration";
+
+      // register to task error
+      this.core.$root.$off(taskAction + "-aborted");
+      this.core.$root.$once(
+        taskAction + "-aborted",
+        this.getConfigurationAborted
+      );
+
+      // register to task completion
+      this.core.$root.$off(taskAction + "-completed");
+      this.core.$root.$once(
+        taskAction + "-completed",
+        this.getConfigurationCompleted
+      );
+
+      const res = await to(
+        this.createModuleTaskForApp(this.instanceName, {
+          action: taskAction,
+          extra: {
+            title: this.$t("action." + taskAction),
+            isNotificationHidden: true,
+          },
+        })
+      );
+      const err = res[0];
+
+      if (err) {
+        console.error(`error creating task ${taskAction}`, err);
+        this.error.getConfiguration = this.getErrorMessage(err);
+        this.loading.getConfiguration = false;
+        return;
+      }
+    },
+    getConfigurationAborted(taskResult, taskContext) {
+      console.error(`${taskContext.action} aborted`, taskResult);
+      this.error.getConfiguration = this.core.$t("error.generic_error");
+      this.loading.getConfiguration = false;
+    },
+    getConfigurationCompleted(taskContext, taskResult) {
+      this.loading.getConfiguration = false;
+      const config = taskResult.output;
+
+      // TODO set configuration fields
+      // ...
+
+      // TODO remove
+      console.log("config", config);
+
+      // TODO focus first configuration field
+      this.focusElement("testField");
+    },
+    validateConfigureModule() {
+      this.clearErrors(this);
+      let isValidationOk = true;
+
+      // TODO remove testField and validate configuration fields
+      if (!this.testField) {
+        // test field cannot be empty
+        this.error.testField = this.$t("common.required");
+
+        if (isValidationOk) {
+          this.focusElement("testField");
+          isValidationOk = false;
+        }
+      }
+      return isValidationOk;
+    },
+    configureModuleValidationFailed(validationErrors) {
+      this.loading.configureModule = false;
+
+      for (const validationError of validationErrors) {
+        const param = validationError.parameter;
+
+        // set i18n error message
+        this.error[param] = this.$t("settings." + validationError.error);
+      }
+    },
+    async configureModule() {
+      const isValidationOk = this.validateConfigureModule();
+      if (!isValidationOk) {
+        return;
+      }
+
+      this.loading.configureModule = true;
+      const taskAction = "configure-module";
+
+      // register to task error
+      this.core.$root.$off(taskAction + "-aborted");
+      this.core.$root.$once(
+        taskAction + "-aborted",
+        this.configureModuleAborted
+      );
+
+      // register to task validation
+      this.core.$root.$off(taskAction + "-validation-failed");
+      this.core.$root.$once(
+        taskAction + "-validation-failed",
+        this.configureModuleValidationFailed
+      );
+
+      // register to task completion
+      this.core.$root.$off(taskAction + "-completed");
+      this.core.$root.$once(
+        taskAction + "-completed",
+        this.configureModuleCompleted
+      );
+
+      const res = await to(
+        this.createModuleTaskForApp(this.instanceName, {
+          action: taskAction,
+          data: {
+            // TODO configuration fields
+          },
+          extra: {
+            title: this.$t("settings.configure_instance", {
+              instance: this.instanceName,
+            }),
+            description: this.$t("common.processing"),
+          },
+        })
+      );
+      const err = res[0];
+
+      if (err) {
+        console.error(`error creating task ${taskAction}`, err);
+        this.error.configureModule = this.getErrorMessage(err);
+        this.loading.configureModule = false;
+        return;
+      }
+    },
+    configureModuleAborted(taskResult, taskContext) {
+      console.error(`${taskContext.action} aborted`, taskResult);
+      this.error.configureModule = this.core.$t("error.generic_error");
+      this.loading.configureModule = false;
+    },
+    configureModuleCompleted() {
+      this.loading.configureModule = false;
+
+      // reload configuration
+      this.getConfiguration();
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "../styles/carbon-utils";
+</style>
