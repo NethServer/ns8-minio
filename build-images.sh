e2188c729813aeaaa9d13e88fccedd4903d9917a
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2023-02-22 15:57:31 +0100

    build: fix ui build

--- build-images.sh
+++ build-images.sh
@@ -20,7 +20,11 @@ if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-mini
 fi
 
 echo "Build static UI files with node..."
-buildah run nodebuilder-minio sh -c "cd /usr/src/ui && yarn install && yarn build"
+buildah run \
+    --workingdir=/usr/src/ui \
+    --env="NODE_OPTIONS=--openssl-legacy-provider" \
+    nodebuilder-minio \
+    sh -c "yarn install && yarn build"
 
 # Add imageroot directory to the container image
 buildah add "${container}" imageroot /imageroot
