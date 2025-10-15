cd71b8f8738c6d763edaee83dab652eca842bbba
Stephane de Labrusse <stephdl@de-labrusse.fr>
2025-10-15 16:46:11 +0200

    fix: update min-core label in build script

--- build-images.sh
+++ build-images.sh
@@ -34,6 +34,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@any:routeadm" \
     --label="org.nethserver.tcp-ports-demand=2" \
     --label="org.nethserver.rootfull=0" \
+    --label="org.nethserver.min-core=3.12.4-0" \
     --label="org.nethserver.images=quay.io/minio/minio:RELEASE.2023-08-23T10-07-06Z" \
     "${container}"
 # Commit the image
