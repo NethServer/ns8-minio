b757a9e121b59e695c0b7156319d99085d72acd3
Stephane de Labrusse <stephdl@de-labrusse.fr>
2025-10-23 15:59:14 +0200

    feat: enhance error notification display with detailed validation error messages

--- ui/src/views/Settings.vue
+++ ui/src/views/Settings.vue
@@ -137,10 +137,22 @@
               <cv-column>
                 <NsInlineNotification
                   kind="error"
-                  :title="core.$t('apps_lets_encrypt.cannot_obtain_certificate')"
-                  :description="formattedValidationErrorDetails"
+                  :title="
+                    core.$t('apps_lets_encrypt.cannot_obtain_certificate')
+                  "
                   :showCloseButton="false"
-                />
+                >
+                  <template #description>
+                    <div class="flex flex-col gap-2">
+                      <div
+                        v-for="(detail, index) in validationErrorDetails"
+                        :key="index"
+                      >
+                        {{ detail }}
+                      </div>
+                    </div>
+                  </template>
+                </NsInlineNotification>
               </cv-column>
             </cv-row>
             <NsButton
@@ -214,9 +226,6 @@ export default {
         this.loading.configureModule ||
         this.loading.getStatus
       );
-    },
-    formattedValidationErrorDetails() {
-      return this.validationErrorDetails.join("\n");
     },
   },
   beforeRouteEnter(to, from, next) {
